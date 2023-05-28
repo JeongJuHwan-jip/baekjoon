7d2339daa969d092077a5ea78dad70512886a7e2
JeongJuHwan-jip <55078363+JeongJuHwan-jip@users.noreply.github.com>
2023-05-28 22:02:05 +0900

    [Bronze IV] Title: 기찍 N, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int T;
+    cin >> T;
+    for( int i = T ; i >= 1 ; --i ) {
+        cout << i << "\n";
+    }
+
+    return 0;
+}
