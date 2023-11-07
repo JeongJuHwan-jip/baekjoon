a20da8b36fd8fef40f2679fac03f3397f9dd4f68
정주환 <55078363+JeongJuHwan-jip@users.noreply.github.com>
2023-11-08 00:42:59 +0900

    [Silver III] Title: Parity Constraint Closest Pair (Easy), Time: 108 ms, Memory: 6380 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/27931. Parity Constraint Closest Pair （Easy）/Parity Constraint Closest Pair （Easy）.cc
+++ 백준/Silver/27931. Parity Constraint Closest Pair （Easy）/Parity Constraint Closest Pair （Easy）.cc
@@ -1,35 +1,75 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 #include <climits>
 
 using namespace std;
-using ll = long long;
 
 int N;
-int arr[500000];
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
 
     cin >> N;
 
+    vector<int> v1, v2;
+    int tmp;
     for(int i=0; i<N; ++i) {
-        cin >> arr[i];
+        cin >> tmp;
+        if(tmp%2) v1.push_back(tmp);
+        else v2.push_back(tmp);
     }
 
-    int ans[2] = {INT_MAX, INT_MAX};
+    sort(v1.begin(), v1.end());
+    sort(v2.begin(), v2.end());
 
-    for(int i=0; i<N; ++i) {
-        for(int j=i+1; j<N; ++j) {
-            int k = abs(arr[i]-arr[j]);
-            ans[k%2] = min(ans[k%2], k);
+    int ans1 = INT_MAX;
+    int ans2 = INT_MAX;
+
+    for(int i=1; i<v1.size(); ++i) {
+        ans1 = min(ans1, v1[i] - v1[i-1]);
+    }
+    for(int i=1; i<v2.size(); ++i) {
+        ans1 = min(ans1, v2[i] - v2[i-1]);
+    }
+
+    if(ans1 == INT_MAX) ans1 = -1;
+
+    if(v1.empty() || v2.empty()) {
+        ans2 = -1;
+        cout << ans1 << ' ' << ans2;
+        return 0;
+    }
+
+    int i1 = 0;
+    int i2 = 0;
+    while(i1 < v1.size() && i2 < v2.size()) {
+        int t1 = v1[i1];
+        int t2 = v2[i2];
+        if(t1 > t2) {
+            i2++;
+        }
+        else {
+            ans2 = min(ans2, t2 - t1);
+            i1++;
         }
     }
 
-    for(int i=0; i<2; ++i) {
-        if(ans[i] == INT_MAX) ans[i] = -1;
-        cout << ans[i] << ' ';
+    i1 = 0;
+    i2 = 0;
+    while(i1 < v1.size() && i2 < v2.size()) {
+        int t1 = v1[i1];
+        int t2 = v2[i2];
+        if(t1 < t2) {
+            i1++;
+        }
+        else {
+            ans2 = min(ans2, t1 - t2);
+            i2++;
+        }
     }
+
+    cout << ans1 << ' ' << ans2;
 
     return 0;
 }
