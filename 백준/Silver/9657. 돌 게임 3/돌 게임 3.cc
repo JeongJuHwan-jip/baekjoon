7681c8dd9a4af992f061d9fda344728461460b19
정주환 <55078363+JeongJuHwan-jip@users.noreply.github.com>
2023-11-11 18:10:11 +0900

    [Silver III] Title: 돌 게임 3, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int N;
+
+int SK(int n);
+int CY(int n);
+
+int cache1[1001];
+int SK(int n) {
+    if(n == 1 || n == 3 || n == 4) return 1;
+    
+    int &ret = cache1[n];
+    if(ret != -1) return ret;
+    
+    ret = !CY(n-1);
+    if(n-3 >= 1) ret |= !CY(n-3);
+    if(n-4 >= 1) ret |= !CY(n-4);
+    
+    return ret;
+}
+
+int cache2[1001];
+int CY(int n) {
+    if(n == 1 || n == 3 || n == 4) return 1;
+    
+    int &ret = cache2[n];
+    if(ret != -1) return ret;
+    
+    ret = !SK(n-1);
+    if(n-3 >= 1) ret |= !SK(n-3);
+    if(n-4 >= 1) ret |= !SK(n-4);
+    
+    return ret;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    cin >> N;
+
+    memset(cache1, -1, sizeof(cache1));
+    memset(cache2, -1, sizeof(cache2));
+
+    if(SK(N)) cout << "SK";
+    else cout << "CY";
+
+    return 0;
+}
